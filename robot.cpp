a5d4df0f2f626c2eb5c48e0b0c07d2ea041c1478
Aaris.Baskaran <aaris.baskaran@gmail.com>
2020-06-06 16:12:48 +1200

    Updated robot.cpp
    Fixed the errors found by Aidan.
    Now we need to finish the findError function, 
    find difference in speed according to error
    then calculate and set speed of motors accordingly.
    Hopefully it'll be pretty easy

--- robot.cpp
+++ robot.cpp
@@ -3,58 +3,82 @@
 
 //check for white pixels
 int isWhite (int pixelValue){
-
+	
+	if (pixelValue > 250){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+	
 }
 
 //find dicstance from white line to center of image
-findError(int arrayCenter, int lineCenter){
-
+int findError(int arrayCenter, int lineCenter){
+	return (arrayCenter - lineCenter);
 }
 
 int main(){
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-
+	
 	//Declare/Initialise variables
-    double vLeft = 20.0;	//left motor speed
-    double vRight = 20.0;	//right motor speed
-
-    int vForward:		//Constant speed of robot to go forward
-	int vDifference:	//Difference between motor speeds
-
+    double vLeft = 10.0;	//left motor speed
+    double vRight = 10.0;	//right motor speed
+    
+    int vForward;		//Constant speed of robot to go forward
+	int vDifference;	//Difference between motor speeds
+    
     int row = 50; 	//row number
     int error;		//distance of line from center
-
-    int* pixArray = new int[150];	//Create array for pixel values
-
-
+    
+    int arraySize = 150;				//Size of array
+    int* pixArray = new int[arraySize];	//Create array for pixel values
+    int sumIndexes = 0;
+    int count = 0;
+    
     //main loop
     while(1){
-
+		
 		takePicture();						//get picture
 		SavePPMFile("i0.ppm",cameraView);	//save picture
-
+		
 		//scan a row of pixels
 		for( int column = 0; column < 150; column++){
 			int pix = get_pixel(cameraView,row,column,3);
 			int pixValue = isWhite(pix);					//call isWhite function
-
+			
 			std::cout<<pixValue<<" ";
-
-			//insert the pixValue into the array
-
+			
+			pixArray[column] = pixValue;
+			
 		} // close for
-
+		
+		//Find sum and number of indexes of white pixels in array
+		sumIndexes = 0;
+		count = 0;
+		for (int i = 0; i < arraySize; i++){
+			if (pixArray[i] == 1){
+				sumIndexes = sumIndexes + i;
+				count++;
+			}
+		}
+		
+		//Find error(findError function still isn't done)
+		error = findError(arraySize/2, sumIndexes/count);
+		std::cout<<"error = "<<error<<std::endl; 
+		
 		// To do:
-		//find error - distance from center
 		//find difference in speed according to error
-		//set speed of motors accordingly
-
+		//calculate and set speed of motors accordingly
+		
 		setMotors(vLeft,vRight);   
 		std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
 		usleep(10000);
+		
   } // close while
 
 } // close main
+
 
